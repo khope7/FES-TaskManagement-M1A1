c586430033cdc1d0fe5aebd3e0f660c5f5a411bf
Daniel Erazo <danielmauricioerazoespinoza@gmail.com>
2025-04-28 21:36:13 -0500

    Fix

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,17 +1,20 @@
 import React, {useContext } from "react";
-import TaskContext from "../context/TasksContext";
+import {TaskContext} from "../context/TasksContext";
 import { Container } from "react-bootstrap";
 
 const TodoList: React.FC = () => {
   const { List } = useContext(TaskContext)
-
   console.log(List.TaskList)
 
   return(
     <div>
       <Container className="mt-5">
         <h1>Task-List:</h1>
-        <h1>{List.TaskList}</h1>
+        {List.TaskList.map((task) => (
+            <div key={task.id}>
+                <h2>{task.task}</h2>
+            </div>
+        ))}
       </Container>
     </div>
   )
